33021fb815b71aff2d05fcd439d3f7bd0beb0e04
Curiouspaul1 <170408025@live.unilag.edu.ng>
2020-05-17 23:31:33 +0100

    added cors to routes

--- Fahead/__init__.py
+++ Fahead/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from config import config
 from flask_mail import Mail, Message
+from flask_cors import CORS
 import os
 
 
@@ -12,6 +13,8 @@ app.config.from_object(config[os.getenv('FLASK_ENV') or 'default'])
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 mail = Mail(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+logging.getLogger('flask_cors').level = logging.DEBUG
 
 # register blueprints
 from .main import api
